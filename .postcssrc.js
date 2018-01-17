ef7a05ff840529d5771b64751acaff44691d41e0
digua <hellodigua@gmail.com>
2018-01-18 00:29:34 +0800

    add postcss

--- .postcssrc.js
+++ .postcssrc.js
@@ -5,6 +5,7 @@ module.exports = {
     "postcss-import": {},
     "postcss-url": {},
     // to edit target browsers: use "browserslist" field in package.json
-    "autoprefixer": {}
+    "autoprefixer": {},
+    "postcss-salad": {}
   }
 }
--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
   </div>
 </template>
 
@@ -11,13 +9,9 @@ export default {
 }
 </script>
 
-<style>
+<style lang="postcss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
+  width: 100%;
 }
 </style>
