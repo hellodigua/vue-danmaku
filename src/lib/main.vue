d6a0dac18595ab05cbed2a2303d2dfe3af9d4146
digua <hellodigua@gmail.com>
2018-02-24 21:38:49 +0400

    add danmu to render

--- src/lib/main.vue
+++ src/lib/main.vue
@@ -45,8 +45,11 @@ export default {
   methods: {
     initDanmu () {
       this.danmu = new Vue(Danmu).$mount('.danmus')
-      this.danmu.danmus = this.danmus
+      // this.danmu.danmus = this.danmus
+      this.danmu.danmus = []
       this.container = this.$refs.container
+      // console.log(this.container.offsetWidth)
+      // console.log(this.container.offsetHeight)
     },
     onMouseenter () {
       if (this.hover) {
@@ -60,23 +63,30 @@ export default {
     },
     draw () {
       this.$nextTick(() => {
-        console.log(this.container.offsetWidth)
-        console.log(this.container.offsetHeight)
         this.timer = setInterval(() => {
           if (this.index > this.danmus.length - 1) {
             clearInterval(this.timer)
           } else {
-            let el = this.danmu.$refs['dm-' + this.index]
-            let width = el.offsetWidth
-            console.log(el.offsetHeight)
-            el.style.left = -width + 'px'
-            el.style.transition = 'left ' + this.speed + 's linear'
-            this.index++
+            this.add()
           }
         }, 1000 * 1.5)
         for (let item in this.danmu.$refs) {
           console.log(this.danmu.$refs[item].offsetWidth)
         }
+      })
+    },
+    add () {
+      this.danmu.danmus.push(this.danmus[this.index])
+      this.$nextTick(() => {
+        console.log(this.danmu.$refs)
+        console.log(this.index)
+        let el = this.danmu.$refs['dm-' + this.index]
+        console.log(el)
+        let width = el.offsetWidth
+        // console.log(el.offsetHeight)
+        el.style.left = -width + 'px'
+        el.style.transition = 'left ' + this.speed + 's linear'
+        this.index++
       })
     },
     stop () {
