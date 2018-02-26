f01230ef174b0dcc4a2fa23ab682aef7fb7c3e8c
digua <digua.wang@dbappsecurity.com.cn>
2018-02-26 22:41:35 +0500

    check collision

--- src/lib/main.vue
+++ src/lib/main.vue
@@ -17,7 +17,14 @@ export default {
       type: Number,
       default: 3
     },
-    config: Object,
+    config: {
+      type: Object,
+      default: () => {
+        return {
+          channels: 0
+        }
+      }
+    },
     hover: {
       type: Boolean,
       default: true
@@ -31,6 +38,7 @@ export default {
       danmaku: null,
       index: 0,
       speed: 5,
+      continueG: true,
       danChannel: {},
       channels: 1, // 轨道数量
       height: 30 // 弹幕高度
@@ -51,9 +59,7 @@ export default {
       this.danmaku = new Vue(Danmu).$mount('.danmus')
       this.danmaku.danmus = []
       this.container = this.$refs.container
-      this.channels = this.config.channels ? this.config.channels : parseInt(this.container.offsetHeight / this.height) // 初始化轨道数量
-      // console.log(this.container.offsetWidth)
-      // console.log(this.container.offsetHeight)
+      this.channels = this.config.channels || parseInt(this.container.offsetHeight / this.height) // 初始化轨道数量
     },
     onMouseenter () {
       if (this.hover) {
@@ -73,52 +79,60 @@ export default {
           } else {
             this.add()
           }
-        }, 1000)
-        for (let item in this.danmaku.$refs) {
-          console.log(this.danmaku.$refs[item].offsetWidth)
-        }
+        }, 100)
       })
     },
     add () {
-      this.danmaku.danmus.push(this.danmus[this.index])
+      if (this.continueG) {
+        this.danmaku.danmus.push(this.danmus[this.index])
+      }
       this.$nextTick(() => {
-        console.log(this.danmaku.$refs)
-        console.log(this.index)
-        let el = this.danmaku.$refs['dm-' + this.index]
-        let tunnelIndex = this.getChannel(el)
+        const el = this.danmaku.$refs['dm-' + this.index]
+        const tunnelIndex = this.getChannel(el)
         console.log(el)
-        let width = el.offsetWidth
-        // console.log(el.offsetHeight)
-        el.style.top = tunnelIndex * this.height + 'px'
-        el.style.left = -width + 'px'
-        el.style.transition = 'left ' + this.speed + 's linear'
-        this.index++
+        if (tunnelIndex >= 0) {
+          this.continueG = true
+          const width = el.offsetWidth
+          // console.log(el.offsetHeight)
+          el.style.top = tunnelIndex * this.height + 'px'
+          el.style.left = -width + 'px'
+          el.style.transition = 'left ' + this.speed + 's linear'
+          this.index++
+        } else {
+          this.continueG = false
+        }
       })
     },
     getChannel (el) {
+      console.log(el.style)
       for (let i = 0; i < this.channels; i++) {
         const items = this.danChannel[i + '']
-        console.log(items)
         if (items && items.length) {
           for (let j = 0; j < items.length; j++) {
+            const danRight = this.getDanRight(items[j]) - 10
             // TODO：碰撞检测
-            if (j === 0) {
+            if (danRight <= 0) {
               break
             }
             if (j === items.length - 1) {
               this.danChannel[i + ''].push(el)
               el.addEventListener('animationend', () => {
-                this.danTunnel[i + ''].splice(0, 1)
+                this.danChannel[i + ''].splice(0, 1)
               })
+              return i % this.channels
             }
           }
         } else {
-          console.log(this.danChannel)
           this.danChannel[i + ''] = [el]
-          return i
+          return i % this.channels
         }
       }
-      return 1
+      return -1
+    },
+    getDanRight (el) {
+      const eleWidth = el.offsetWidth || parseInt(el.style.width)
+      const eleRight = el.getBoundingClientRect().right || this.container.getBoundingClientRect().right + eleWidth
+      return this.container.getBoundingClientRect().right - eleRight
     },
     stop () {
     }
