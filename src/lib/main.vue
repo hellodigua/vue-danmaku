4175f562f01c067309f01dfce62bf7433cb4cd37
digua <hellodigua@gmail.com>
2021-01-16 12:04:34 +0800

    feat: support slot

--- src/lib/main.vue
+++ src/lib/main.vue
@@ -3,12 +3,14 @@
        class="vue-danmaku"
        @mouseenter="mouseIn"
        @mouseleave="mouseOut">
-    <slot></slot>
     <div :class="['danmus', {'show': !hidden}, {'paused': paused}]"
          ref="danmus"></div>
+    <slot name="content" />
   </div>
 </template>
 <script>
+import Vue from 'vue'
+
 export default {
   props: {
     danmus: {
@@ -33,17 +35,17 @@ export default {
         danmus: [],
         width: 0, // danmaku宽度
         channels: 0, // 轨道数量
-        loop: false // 是否循环
+        loop: false, // 是否循环
+        slot: false // 是否开启slot
       },
       danmu: {
-        height: 30,
+        height: 0,
         fontSize: 18,
         speed: 5
       },
+      index: 0,
       hidden: false,
       paused: false,
-      index: 0,
-      continue: true,
       danChannel: {}
     }
   },
@@ -62,7 +64,9 @@ export default {
       this.initConfig()
     },
     reset() {
-      this.initConfig()
+      this.$danmaku = null
+      this.$danmus = null
+      this.init()
     },
     mouseIn() {
       this.$emit('mouseIn')
@@ -73,15 +77,17 @@ export default {
     initCore() {
       this.$danmaku = this.$refs.danmaku
       this.$danmus = this.$refs.danmus
-    },
-    initConfig() {
       this.danmaku.width = this.$danmaku.offsetWidth
       this.danmaku.height = this.$danmaku.offsetHeight
+    },
+    initConfig() {
       this.danmaku.danmus = this.danmus
-      this.danmaku.channels = this.config.channels || parseInt(this.danmaku.height / this.danmu.height)
-      this.danmaku.loop = this.config.loop || this.danmaku.loop
-      this.danmu.speed = this.config.speed || this.danmu.speed
-      this.danmu.fontSize = this.config.fontSize || this.danmu.fontSize
+      const { channels = 3, loop = false, slot = false, speed = 5, fontSize = 18 } = this.config
+      this.danmaku.channels = channels || parseInt(this.danmaku.height / this.danmu.height)
+      this.danmaku.loop = loop
+      this.danmaku.slot = slot
+      this.danmu.speed = speed
+      this.danmu.fontSize = fontSize
     },
     play() {
       if (this.paused) {
@@ -96,7 +102,7 @@ export default {
       this.$nextTick(() => {
         this.timer = setInterval(() => {
           if (this.index > this.danmus.length - 1) {
-            this.config.loop ? this.insert() : this.clear()
+            this.danmaku.loop ? this.insert() : this.clear()
           } else {
             this.insert()
           }
@@ -104,23 +110,28 @@ export default {
       })
     },
     insert() {
-      const index = this.config.loop ? this.index % this.danmus.length : this.index
-      const el = document.createElement(`p`)
-      if (this.continue) {
-        el.classList.add(`move`)
-        el.style.animationDuration = `${this.danmu.speed}s`
-        el.style.fontSize = `${this.danmu.fontSize}px`
-        el.innerHTML = this.danmus[index]
-        el.setAttribute('index', this.index)
-        this.$danmus.appendChild(el)
+      const index = this.danmaku.loop ? this.index % this.danmus.length : this.index
+      let el = document.createElement(`div`)
+      // console.log(this.danmaku.slot)
+      if (this.danmaku.slot) {
+        el = this.getSlotComponent(index).$el
       }
+      if (!this.danmaku.slot) {
+        el.innerHTML = this.danmus[index]
+      }
+      el.classList.add('dm', 'move')
+      el.style.animationDuration = `${this.danmu.speed}s`
+      el.style.fontSize = `${this.danmu.fontSize}px`
+      el.setAttribute('index', this.index)
+      this.$danmus.appendChild(el)
       this.$nextTick(() => {
+        if (!this.danmu.height || !this.danmu.width) {
+          this.danmu.height = el.offsetHeight
+        }
         let channelIndex = this.getChannel(el)
         if (channelIndex >= 0) {
-          this.continue = true
           const width = el.offsetWidth
           const height = this.danmu.height > this.danmu.fontSize ? this.danmu.height : this.danmu.fontSize + 4
-          el.classList.add(`dm`)
           el.style.top = channelIndex * height + 'px'
           el.style.width = width + 1 + 'px'
           el.style.transform = `translateX(-${this.danmaku.width}px)`
@@ -136,6 +147,32 @@ export default {
           }
         }
       })
+    },
+    getSlotComponent(index) {
+      const that = this
+      const DmComponent = Vue.extend({
+        props: {
+          danmu: [String, Object],
+          index: Number
+        },
+        render(createElement) {
+          return createElement('div', [
+            that.$scopedSlots.default({
+              danmu: this.danmu,
+              index: this.index
+            })
+          ])
+        }
+      })
+
+      const ele = new DmComponent({
+        propsData: {
+          danmu: this.danmus[index],
+          index
+        }
+      }).$mount(document.createElement('div'))
+
+      return ele
     },
     getChannel(el) {
       const tmp = this.$danmus.offsetWidth / ((this.$danmus.offsetWidth + el.offsetWidth) / 6)
@@ -186,6 +223,9 @@ export default {
       this.hidden = false
       this.clear()
     },
+    getPlayStatus() {
+      return !this.paused
+    },
     clear() {
       clearInterval(this.timer)
       this.timer = null
@@ -220,6 +260,7 @@ export default {
     opacity: 0;
     -webkit-transition: all 0.3s;
     transition: all 0.3s;
+    z-index: 1000;
     &.show {
       opacity: 1;
     }
