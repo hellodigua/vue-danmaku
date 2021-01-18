0b0f4c3ae77578b0ac3a2c50ba86168a1b2ad0c5
digua <hellodigua@gmail.com>
2021-01-18 20:50:59 +0800

    chore: upgrade vue-cli

---
+++ vue.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  parallel: false,
+  publicPath:
+    process.env.NODE_ENV === 'production' && process.env.VUE_APP_BUILD_MODE !== 'package' ? '/vue-danmaku/' : '/',
+  outputDir: process.env.VUE_APP_BUILD_MODE === 'package' ? 'dist' : 'docs',
+  chainWebpack: (config) => {
+    if (process.env.VUE_APP_BUILD_MODE !== 'package') {
+      config.resolve.alias.set('~', __dirname)
+    } else {
+      config.output.libraryExport('default')
+      config.externals({
+        vue: {
+          commonjs: 'vue',
+          commonjs2: 'vue',
+          root: 'Vue',
+          amd: 'vue',
+        },
+      })
+    }
+  },
+  css: { extract: !!process.env.NO_EXTRACT_CSS },
+}
